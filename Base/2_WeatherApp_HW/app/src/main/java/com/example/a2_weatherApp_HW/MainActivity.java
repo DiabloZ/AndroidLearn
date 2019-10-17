daa9337f329bfe7fd13eb2fe5452d57b68d454d0
DiabloZ <diabloz@me.com>
2019-10-17 17:57:52 +0300

    Закончил делать домашнюю работу.

--- Base/2_WeatherApp_HW/app/src/main/java/com/example/a2_weatherApp_HW/MainActivity.java
+++ Base/2_WeatherApp_HW/app/src/main/java/com/example/a2_weatherApp_HW/MainActivity.java
@@ -4,47 +4,95 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.example.a2_weatherApp_HW.SetupActivity.DATA_FOR_RETURN_KEY;
+import static com.example.a2_weatherApp_HW.SetupActivity.DATA_FOR_RETURN_KEY_BUNDLE;
+import static com.example.a2_weatherApp_HW.SetupActivity.DATA_KEY_CHANGE_OF_RAIN;
+import static com.example.a2_weatherApp_HW.SetupActivity.DATA_KEY_HUMIDITY;
+import static com.example.a2_weatherApp_HW.SetupActivity.DATA_KEY_WIND;
+
 public class MainActivity extends AppCompatActivity {
-    private Button btnSetup;
+    private final int SECOND_ACTIVITY_REQUEST_CODE = 123;
 
-    private TextView currentCity;
+    private TextView currentCity, chanceOfRain, speedWind, humidity;
 
-    private final int secondActivityRequestCode = 123;
+    private LinearLayout upPanelOptional;
+
+    protected static final String SEND_SWITCH_TO_SETUP = "sendCode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-        setToSetupActivityClickBehaviour();
     }
 
     private void initViews() {
-        btnSetup = findViewById(R.id.btnSetup);
         currentCity = findViewById(R.id.currentCity);
-    }
+        chanceOfRain = findViewById(R.id.chanceOfRain);
+        speedWind = findViewById(R.id.speedWind);
+        humidity = findViewById(R.id.humidity);
+        upPanelOptional = findViewById(R.id.upPanelOptional);
 
-    private void setToSetupActivityClickBehaviour() {
+        Button btnSetup = findViewById(R.id.btnSetup);
         btnSetup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SetupActivity.class);
-                startActivityForResult(intent, secondActivityRequestCode);
+                setToSetupActivityClickBehaviour();
             }
         });
+    }
+
+    private void setToSetupActivityClickBehaviour() {
+        Intent data = new Intent(MainActivity.this, SetupActivity.class);
+        Bundle arguments = getSwitchBundle();
+        data.putExtra(SEND_SWITCH_TO_SETUP, arguments);
+        setResult(RESULT_OK);
+        startActivityForResult(data, SECOND_ACTIVITY_REQUEST_CODE, arguments);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == secondActivityRequestCode && resultCode == RESULT_OK){
-            String text = data != null ? data.getStringExtra(SetupActivity.dataForReturnKey) : "";
-            currentCity.setText(text);
+        if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
+            String text = data != null ? data.getStringExtra(DATA_FOR_RETURN_KEY) : "";
+            switch (resultCode) {
+                case (RESULT_OK):
+                    Bundle bundle = data != null ? data.getBundleExtra(DATA_FOR_RETURN_KEY_BUNDLE) : null;
+                    if (bundle != null) switchChange(bundle);
+                    if (!text.equals("")) currentCity.setText(text);
+                    break;
+                case (RESULT_CANCELED):
+                    break;
+            }
         }
+    }
+
+    private void switchChange(Bundle bundle) {
+        boolean switchChangeOfRain = bundle.getBoolean(DATA_KEY_CHANGE_OF_RAIN);
+        boolean switchWind = bundle.getBoolean(DATA_KEY_WIND);
+        boolean switchHumidity = bundle.getBoolean(DATA_KEY_HUMIDITY);
+
+        chanceOfRain.setVisibility(switchChangeOfRain ? View.VISIBLE : View.GONE);
+        speedWind.setVisibility(switchWind ? View.VISIBLE : View.GONE);
+        humidity.setVisibility(switchHumidity ? View.VISIBLE : View.GONE);
+        upPanelOptional.setVisibility(checkVisibilityUpPanel() ? View.GONE : View.VISIBLE);
+    }
+
+    private boolean checkVisibilityUpPanel() {
+        return chanceOfRain.getVisibility() == View.GONE && speedWind.getVisibility() == View.GONE && humidity.getVisibility() == View.GONE;
+    }
+
+    private Bundle getSwitchBundle() {
+        Bundle arguments = new Bundle();
+        arguments.putBoolean(DATA_KEY_HUMIDITY, humidity.getVisibility() == View.VISIBLE);
+        arguments.putBoolean(DATA_KEY_WIND, speedWind.getVisibility() == View.VISIBLE);
+        arguments.putBoolean(DATA_KEY_CHANGE_OF_RAIN, chanceOfRain.getVisibility() == View.VISIBLE);
+        return arguments;
     }
 }
--- Base/2_WeatherApp_HW/app/src/main/java/com/example/a2_weatherApp_HW/SetupActivity.java
+++ Base/2_WeatherApp_HW/app/src/main/java/com/example/a2_weatherApp_HW/SetupActivity.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SetupActivity extends AppCompatActivity {
+
+    protected final static String DATA_FOR_RETURN_KEY = "dataForReturn";
+    protected final static String DATA_FOR_RETURN_KEY_BUNDLE = "DATA_FOR_RETURN_KEY_BUNDLE";
+    protected final static String DATA_KEY_CHANGE_OF_RAIN = "DATA_KEY_CHANGE_OF_RAIN";
+    protected final static String DATA_KEY_WIND = "DATA_KEY_WIND";
+    protected final static String DATA_KEY_HUMIDITY = "DATA_KEY_HUMIDITY";
+
     private EditText editChangeCity;
     private Button okBtn, cancelBtn;
-
-    final static String dataForReturnKey = "dataForReturn";
-    private String isCancelString = "Вы отменили ввод города :(";
+    private Switch switchChangeOfRain, switchWind, switchHumidity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,22 +28,42 @@ public class SetupActivity extends AppCompatActivity {
         setContentView(R.layout.setup_activity);
         initViews();
         setBehaviourForMainActBtn();
+        intentParse(getIntent());
+    }
+
+    private void intentParse(Intent intent) {
+        Bundle bundle = intent.getBundleExtra(MainActivity.SEND_SWITCH_TO_SETUP);
+        if (bundle != null) {
+            switchMemory(bundle);
+        }
     }
 
 
+    private void switchMemory(Bundle bundle) {
+        boolean changeOfRain = bundle.getBoolean(DATA_KEY_CHANGE_OF_RAIN);
+        boolean wind = bundle.getBoolean(DATA_KEY_WIND);
+        boolean humidity = bundle.getBoolean(DATA_KEY_HUMIDITY);
+        if (!changeOfRain) switchChangeOfRain.setChecked(false);
+        if (!wind) switchWind.setChecked(false);
+        if (!humidity) switchHumidity.setChecked(false);
+    }
 
     private void initViews() {
         editChangeCity = findViewById(R.id.changeCity);
         okBtn = findViewById(R.id.okMenu);
         cancelBtn = findViewById(R.id.cancelMenu);
+        switchChangeOfRain = findViewById(R.id.switchChanceOfRain);
+        switchWind = findViewById(R.id.switchWind);
+        switchHumidity = findViewById(R.id.switchHumidity);
     }
+
     private void setBehaviourForMainActBtn() {
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String text = editChangeCity.getText().toString();
-                Intent data = new Intent();
-                data.putExtra(dataForReturnKey, text);
+                Intent data = getIntentToSend();
+                data.putExtra(DATA_FOR_RETURN_KEY, text);
                 setResult(RESULT_OK, data);
                 finish();
             }
@@ -45,12 +71,25 @@ public class SetupActivity extends AppCompatActivity {
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text = isCancelString;
-                Intent data = new Intent();
-                data.putExtra(dataForReturnKey, text);
+                Intent data = getIntentToSend();
                 setResult(RESULT_CANCELED, data);
                 finish();
             }
         });
+    }
+
+    private Intent getIntentToSend() {
+        Intent data = new Intent();
+        Bundle arguments = getSwitchBundle();
+        data.putExtra(DATA_FOR_RETURN_KEY_BUNDLE, arguments);
+        return data;
+    }
+
+    private Bundle getSwitchBundle() {
+        Bundle arguments = new Bundle();
+        arguments.putBoolean(DATA_KEY_CHANGE_OF_RAIN, switchChangeOfRain.isChecked());
+        arguments.putBoolean(DATA_KEY_WIND, switchWind.isChecked());
+        arguments.putBoolean(DATA_KEY_HUMIDITY, switchHumidity.isChecked());
+        return arguments;
     }
 }
