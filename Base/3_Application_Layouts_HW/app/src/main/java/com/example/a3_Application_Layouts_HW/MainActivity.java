4774fe7f854d446b7664c303977abd8ebe54ac6e
DiabloZ <diabloz@me.com>
2019-10-21 19:33:16 +0300

    Закончил над третим домашним заданием.

--- Base/3_Application_Layouts_HW/app/src/main/java/com/example/a3_Application_Layouts_HW/MainActivity.java
+++ Base/3_Application_Layouts_HW/app/src/main/java/com/example/a3_Application_Layouts_HW/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a3_Application_Layouts_HW;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     private LinearLayout upPanelOptional;
 
+    private static final String SAVE_CITY_NAME = "saveCity";
+    private static final String SAVE_CITY_NAME_BUNDLE = "saveCityBundle";
     protected static final String SAVE_INSTANCE_BUNDLE = "saveBundle";
     protected static final String SEND_SWITCH_TO_SETUP = "sendCode";
 
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        Log.d(this.getClass().getSimpleName(), "успешно запущен");
     }
 
     private void initViews() {
@@ -40,36 +44,44 @@ public class MainActivity extends AppCompatActivity {
         speedWind = findViewById(R.id.speedWind);
         humidity = findViewById(R.id.humidity);
         upPanelOptional = findViewById(R.id.upPanelOptional);
+        Log.d(this.getClass().getSimpleName(), "Views успешно обработаны");
 
         Button btnSetup = findViewById(R.id.btnSetup);
         btnSetup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setToSetupActivityClickBehaviour();
+                createAndOpenNewActivitySetResult();
+                Log.i(this.getClass().getSimpleName(), "кнопка настроек нажата");
             }
         });
     }
 
-    private void setToSetupActivityClickBehaviour() {
+    private void createAndOpenNewActivitySetResult() {
         Intent data = new Intent(MainActivity.this, SetupActivity.class);
         Bundle arguments = getSwitchBundle();
         data.putExtra(SEND_SWITCH_TO_SETUP, arguments);
         setResult(RESULT_OK);
         startActivityForResult(data, SECOND_ACTIVITY_REQUEST_CODE, arguments);
+        Log.i(this.getClass().getSimpleName(), "класс успешно отработал и создал интент");
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.d(this.getClass().getSimpleName(), "приняли результат с второго активити");
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
+            Log.d(this.getClass().getSimpleName(), "вошли внуть IF по реквест коду");
             String text = data != null ? data.getStringExtra(DATA_FOR_RETURN_KEY) : "";
             switch (resultCode) {
                 case (RESULT_OK):
+                    Log.d(this.getClass().getSimpleName(), "вошли в RESULT_OK");
                     Bundle bundle = data != null ? data.getBundleExtra(DATA_FOR_RETURN_KEY_BUNDLE) : null;
                     if (bundle != null) switchChange(bundle);
                     if (!text.equals("")) currentCity.setText(text);
+                    Log.d(this.getClass().getSimpleName(), "бандл не нулевой, вернули строку - " + text);
                     break;
                 case (RESULT_CANCELED):
+                    Log.d(this.getClass().getSimpleName(), "вошли в RESULT_CANCELED");
                     break;
             }
         }
@@ -77,18 +89,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(this.getClass().getSimpleName(), "сохраняем состояние");
+        outState.putBundle(SAVE_CITY_NAME_BUNDLE, getCityName());
         outState.putBundle(SAVE_INSTANCE_BUNDLE, getSwitchBundle());
         super.onSaveInstanceState(outState);
+        Log.d(this.getClass().getSimpleName(), "сохранили состояние");
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(this.getClass().getSimpleName(), "восстанавливаем состояние");
         Bundle bundle = savedInstanceState.getBundle(SAVE_INSTANCE_BUNDLE);
         if (bundle != null) switchChange(bundle);
+        bundle = savedInstanceState.getBundle(SAVE_CITY_NAME_BUNDLE);
+        if (bundle != null) cityNameRestore(bundle);
         super.onRestoreInstanceState(savedInstanceState);
+        Log.d(this.getClass().getSimpleName(), "восстановили состояние");
     }
 
     private void switchChange(Bundle bundle) {
+        Log.d(this.getClass().getSimpleName(), "сохраняем переключатели");
         boolean switchChangeOfRain = bundle.getBoolean(DATA_KEY_CHANGE_OF_RAIN);
         boolean switchWind = bundle.getBoolean(DATA_KEY_WIND);
         boolean switchHumidity = bundle.getBoolean(DATA_KEY_HUMIDITY);
@@ -99,15 +119,31 @@ public class MainActivity extends AppCompatActivity {
         upPanelOptional.setVisibility(checkVisibilityUpPanel() ? View.GONE : View.VISIBLE);
     }
 
+    private void cityNameRestore(Bundle bundle) {
+        Log.d(this.getClass().getSimpleName(), "восстанавливаем город");
+        String cityName = bundle.getString(SAVE_CITY_NAME);
+        currentCity.setText(cityName);
+
+    }
+
     private boolean checkVisibilityUpPanel() {
         return chanceOfRain.getVisibility() == View.GONE && speedWind.getVisibility() == View.GONE && humidity.getVisibility() == View.GONE;
     }
 
     private Bundle getSwitchBundle() {
+        Log.d(this.getClass().getSimpleName(), "пакуем состояние свичей в бандл");
         Bundle arguments = new Bundle();
         arguments.putBoolean(DATA_KEY_HUMIDITY, humidity.getVisibility() == View.VISIBLE);
         arguments.putBoolean(DATA_KEY_WIND, speedWind.getVisibility() == View.VISIBLE);
         arguments.putBoolean(DATA_KEY_CHANGE_OF_RAIN, chanceOfRain.getVisibility() == View.VISIBLE);
+        return arguments;
+    }
+
+    private Bundle getCityName() {
+        Log.d(this.getClass().getSimpleName(), "сохраняем название города");
+        Bundle arguments = new Bundle();
+        String city = currentCity.getText() != null ? String.valueOf(currentCity.getText()) : "";
+        arguments.putString(SAVE_CITY_NAME, city);
         return arguments;
     }
 }
--- Base/3_Application_Layouts_HW/app/src/main/java/com/example/a3_Application_Layouts_HW/SetupActivity.java
+++ Base/3_Application_Layouts_HW/app/src/main/java/com/example/a3_Application_Layouts_HW/SetupActivity.java
@@ -2,6 +2,7 @@ package com.example.a3_Application_Layouts_HW;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +25,7 @@ public class SetupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(this.getClass().getSimpleName(), "запустили второе активити");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setup_activity);
         initViews();
@@ -32,6 +34,7 @@ public class SetupActivity extends AppCompatActivity {
     }
 
     private void intentParse(Intent intent) {
+        Log.d(this.getClass().getSimpleName(), "получаем значения из интента");
         Bundle bundle = intent.getBundleExtra(MainActivity.SEND_SWITCH_TO_SETUP);
         if (bundle != null) {
             switchMemory(bundle);
@@ -40,6 +43,7 @@ public class SetupActivity extends AppCompatActivity {
 
 
     private void switchMemory(Bundle bundle) {
+        Log.d(this.getClass().getSimpleName(), "получаем значение свичей");
         boolean changeOfRain = bundle.getBoolean(DATA_KEY_CHANGE_OF_RAIN);
         boolean wind = bundle.getBoolean(DATA_KEY_WIND);
         boolean humidity = bundle.getBoolean(DATA_KEY_HUMIDITY);
@@ -49,6 +53,7 @@ public class SetupActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        Log.d(this.getClass().getSimpleName(), "инициализируем вьюхи");
         editChangeCity = findViewById(R.id.changeCity);
         okBtn = findViewById(R.id.okMenu);
         cancelBtn = findViewById(R.id.cancelMenu);
@@ -61,6 +66,7 @@ public class SetupActivity extends AppCompatActivity {
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(this.getClass().getSimpleName(), "обрабатываем нажатие на ОК");
                 String text = editChangeCity.getText().toString();
                 Intent data = getIntentToSend();
                 data.putExtra(DATA_FOR_RETURN_KEY, text);
@@ -71,6 +77,7 @@ public class SetupActivity extends AppCompatActivity {
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(this.getClass().getSimpleName(), "обрабатываем нажатие на CANCEL");
                 Intent data = getIntentToSend();
                 setResult(RESULT_CANCELED, data);
                 finish();
@@ -79,6 +86,7 @@ public class SetupActivity extends AppCompatActivity {
     }
 
     private Intent getIntentToSend() {
+        Log.d(this.getClass().getSimpleName(), "создаем intent для отправки");
         Intent data = new Intent();
         Bundle arguments = getSwitchBundle();
         data.putExtra(DATA_FOR_RETURN_KEY_BUNDLE, arguments);
@@ -86,6 +94,7 @@ public class SetupActivity extends AppCompatActivity {
     }
 
     private Bundle getSwitchBundle() {
+        Log.d(this.getClass().getSimpleName(), "отправляем новое состояние свичей");
         Bundle arguments = new Bundle();
         arguments.putBoolean(DATA_KEY_CHANGE_OF_RAIN, switchChangeOfRain.isChecked());
         arguments.putBoolean(DATA_KEY_WIND, switchWind.isChecked());
