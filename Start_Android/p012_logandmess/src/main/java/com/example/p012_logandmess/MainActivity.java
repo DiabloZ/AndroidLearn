5d4220162afe58e645f85e2382ac2badd101c9e0
diabloz <diabloz@me.com>
2020-03-14 23:09:47 +0300

    Доделал.

--- Start_Android/p012_logandmess/src/main/java/com/example/p012_logandmess/MainActivity.java
+++ Start_Android/p012_logandmess/src/main/java/com/example/p012_logandmess/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView tvOut;
@@ -52,6 +53,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 tvOut.setText("Нажата кнопка Cancel");
                 break;
         }
-
+        Toast.makeText(this, tvOut.getText(),Toast.LENGTH_SHORT).show();
     }
 }
