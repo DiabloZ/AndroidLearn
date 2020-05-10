2851003fdc20e33e379bc4a80b8c050dca927e9f
DiabloZ <diabloz@me.com>
2020-05-10 18:44:59 +0300

    Сделал 18 задание

--- Start_Android/p016_dynamiclayout/src/main/java/com/example/p016_dynamiclayout/MainActivity.java
+++ Start_Android/p016_dynamiclayout/src/main/java/com/example/p016_dynamiclayout/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.p016_dynamiclayout;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +18,38 @@ public class MainActivity extends AppCompatActivity {
         // установим вертикальную ориентацию
         linLayout.setOrientation(LinearLayout.VERTICAL);
         // создаем LayoutParams
-        LinearLayout.LayoutParams linLayoutParam =
-            new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-            );
+        LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         // устанавливаем linLayout как корневой элемент экрана
         setContentView(linLayout, linLayoutParam);
+
+        LinearLayout.LayoutParams lpView = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv, lpView);
+
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
+
+
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        leftMarginParams.leftMargin = 50;
+        Button btn1 = new Button(this);
+        btn1.setText("BTN1");
+        linLayout.addView(btn1, leftMarginParams);
+
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button btn2 = new Button(this);
+        btn2.setText("BTN2");
+        linLayout.addView(btn2, rightGravityParams);
     }
 }
